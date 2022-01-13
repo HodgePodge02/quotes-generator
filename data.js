c95a9894a40f58d71ded96577dd27664a3cc4ae8
HodgePodge02 <false4956@gmail.com>
2022-01-12 22:15:03 -0500

    style up page + start data js

---
+++ data.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    quote: "Lorem test hey whats up",
+    author: "Ollie",
+  },
+  {
+    quote: "Lorem adam's quote",
+    author: "Adam",
+  },
+  {
+    quote: "Weddings are just funerals with cake.",
+    author: "Rick Sanchez",
+  },
+];
