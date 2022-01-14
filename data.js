c1783ef7b17c6b2390a796cd0b228cedcd8354fd
HodgePodge02 <false4956@gmail.com>
2022-01-14 17:09:29 -0500

    added more objects to my ever-expanding array.

--- data.js
+++ data.js
@@ -1,14 +1,75 @@
 export default [
   {
-    quote: "Lorem test hey whats up",
+    quote: "Bitches are temporary but the boys are forever...",
     author: "Ollie",
   },
   {
-    quote: "Lorem adam's quote",
+    quote: "Bro... don't post that",
     author: "Adam",
   },
   {
     quote: "Weddings are just funerals with cake.",
     author: "Rick Sanchez",
+  },
+  {
+    quote:
+      "I have learned now that while those who speak about one's miseries usually hurt, those who keep silence hurt more.",
+    author: "C.S. Lewis",
+  },
+  {
+    quote:
+      "So its true, when all is said and done, grief is the price we pay for love.",
+    author: "E.A. Bucchianeri",
+  },
+  {
+    quote:
+      "All of humanity's problems stem from man's inability to sit quietly in a room alone.",
+    author: "Blaise Pasca",
+  },
+  {
+    quote:
+      "People keep telling me that life goes on, but to me, that's the saddest part.",
+    author: "Anonymous",
+  },
+  {
+    quote:
+      "A sad thing in life is that sometimes you meet someone who means a lot to you only to find out in the end that it was never bound to be, and you just have to let go.",
+    author: "Anonymous",
+  },
+  {
+    quote:
+      "My mind became a prison of my own thoughts, drowning in the echoes of my own loneliness",
+    author: "Anonymous",
+  },
+  {
+    quote:
+      "There is no greater sorrow than to recall, in misery, the time when we were happy.",
+    author: "Dante Alighieri",
+  },
+  {
+    quote: "Sometimes you laugh because you've got no more room for crying.",
+    author: "Terry Pratchett",
+  },
+  {
+    quote:
+      "It's better to leave early rather than to feel the pain all the time.",
+    author: "Anonymous",
+  },
+  {
+    quote: "Never put your happiness in someone else's hands.",
+    author: "Anonymous",
+  },
+  {
+    quote: "The heart was made to be broken.",
+    author: "Oscar Wilde",
+  },
+  {
+    quote:
+      "It's amazing how someone can break your heart, and you can still love them with all the little pieces.",
+    author: "Ella Harper",
+  },
+  {
+    quote: "Never love with all your heart. It only ends in breaking.",
+    author: "Anonymous",
   },
 ];
